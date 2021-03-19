28dbd7b6a586bd44c3d700f00c036e7bc193c31b
SojiroPopo <sojiro.popo@gmail.com>
2021-03-19 22:57:39 +0100

    Submit Assignment

---
+++ Tugas1/Soal-1.py
@@ -0,0 +1,12 @@
+# Assignment #1 - Basic Python
+# Yanuar Adrian Bomantara
+# Soal no 1
+
+# Variable input
+Nama = input("Nama :")
+Umur = int(input("Umur :"))
+Tinggi = float(input("Tinggi (cm) :"))
+
+# Print text hasil input
+text = "Nama saya {}, umur saya {} tahun dan tinggi saya {} cm.".format(Nama, Umur, Tinggi)
+print(text)
